396a0e3c5a75fd7111dccfae5401e14050f17744
best99-cloud <67965407+best99-cloud@users.noreply.github.com>
2026-02-08 21:11:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from PIL import Image
+import io
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "Movie Shazam backend running"}
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    content = await file.read()
+    image = Image.open(io.BytesIO(content))
+
+    return {
+        "filename": file.filename,
+        "format": image.format,
+        "size": image.size
+    }
